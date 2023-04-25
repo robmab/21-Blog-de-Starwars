3211f3ddd6b45d83f7092c27e0cb14d9401e0793
robmab <rob_mb@outlook.es>
2023-04-25 15:44:46 +0200

    Home design

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "../../styles/navbar.css";
+
+import sfLogo from "../../img/sw.png";
+
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <div className="">
+        <Link to="/">
+          <img width="80px" src={sfLogo} alt="" />
+        </Link>
+
+        <div className="dropdown">
+          <button
+            className="btn btn-primary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Favorites
+            <div>
+              <p>1</p>
+            </div>
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li>
+              <a className="dropdown-item" href="#">
+                Action
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Another action
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Something else here
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
