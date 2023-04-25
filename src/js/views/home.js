10f81aad7af96965e00f552a06a40da59ca4953e
robmab <rob_mb@outlook.es>
2023-04-25 15:44:35 +0200

    Home design

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,31 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+
 import "../../styles/home.css";
 
+import { Card } from "../component/card.jsx";
+
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+  <div className="home-wrapper">
+    <h1>Characters</h1>
+    <div className="card-wrapper">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    <h1>Vehicles</h1>
+    <div className="card-wrapper">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    <h1>Planets</h1>
+    <div className="card-wrapper">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </div>
 );
