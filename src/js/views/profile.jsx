9f8468cd75ef1c440fc215ebf2bf3fed4b8e7dc2
robmab <rob_mb@outlook.es>
2023-06-09 05:51:37 +0200

    profile update

--- src/js/views/profile.jsx
+++ src/js/views/profile.jsx
@@ -6,7 +6,7 @@ import { Context } from "../store/appContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 
-import "../../styles/formulary.css";
+import "../../styles/profile.css";
 
 export const Profile = () => {
   const { store, actions } = useContext(Context);
@@ -20,23 +20,49 @@ export const Profile = () => {
     const priv = async () => {
       const profile = await actions.private();
       if (!profile) {
-        actions.logout()
-        navigate("/")};
+        actions.logout();
+        navigate("/");
+      }
       setLoad(true);
     };
     priv();
   }, []);
 
   return (
-    <div>
+    <>
       {load && store.user.user_name !== undefined && (
-        <>
-          <p>{store.user.user_name}</p>
-          <p>{store.user.first_name}</p>
-          <p>{store.user.last_name}</p>
-          <p>{store.user.email}</p>
-        </>
+        <div className="profile-wrapper">
+          <h1>User Info</h1>
+          <div className="profile">
+            <div>
+              <p>
+                <strong>Username: </strong>
+                {store.user.user_name}
+              </p>
+            </div>
+            <div>
+              <p>
+                <strong>First name: </strong>
+                {store.user.first_name}
+              </p>
+            </div>
+
+            <div>
+              <p>
+                <strong>Last name: </strong>
+                {store.user.last_name}
+              </p>
+            </div>
+
+            <div>
+              <p>
+                <strong>Email: </strong>
+                {store.user.email}
+              </p>
+            </div>
+          </div>
+        </div>
       )}
-    </div>
+    </>
   );
 };
